f25a554c40c4a4a3e59b06c330142c3b2f279e5f
Fraser Scott <fraser.scott@gmail.com>
2014-11-20 10:37:53 +0000

    version bump for contributions

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,9 @@
 name             'influxdb'
 maintainer       'Fraser Scott'
 maintainer_email 'fraser.scott@gmail.com'
-
 license          'MIT'
 description      'Installs/Configures influxdb'
 long_description 'Installs/Configures influxdb'
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'limits', '~> 0.2'
-
